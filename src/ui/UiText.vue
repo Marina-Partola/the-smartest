8c50314713d36aceaa8f31f65cc4c690f81dae4b
Marina Partola <gma18@list.ru>
2025-02-03 11:03:20 +0500

    add draft of game-view

---
+++ src/ui/UiText.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+type TextVariant = 'title-game' | 'body-game'
+interface Props {
+  variant: TextVariant
+}
+
+const tagByVariant: Record<TextVariant, string> = {
+  'title-game': 'h1',
+  'body-game': 'span',
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <component :is="tagByVariant[variant]" :class="variant"><slot /></component>
+</template>
+
+<style scoped>
+.title-game {
+  font-size: 32px;
+  font-family: 'Montserrat Alternates';
+}
+
+.body-game {
+  font-size: 24px;
+  font-family: 'Montserrat Alternates';
+}
+</style>
